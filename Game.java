6d15ed8fbe1cedb8478ee754f88b4378255d504a
Hannah <hannah.hudson.@techelevator.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[[[C[C[C[C[C[C[C[C[C[C[C[C[C[C@techelevator.com>
2023-01-03 11:58:41 -0500

    updated value map for cards and hit and stay logic

--- Game.java
+++ Game.java
@@ -1,6 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Game {
 
     private Deck deck;
+    private Map<Integer, Integer> cardValues = new HashMap<>();
 
     //TODO: Add chips in the future?
     public static final int NOTHING =0;
@@ -27,6 +31,22 @@ public class Game {
         int[] suits = new int[]{SPADES, DIAMONDS, CLUBS, HEARTS};
         int[] ranks = new int[]{ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
         deck = new Deck(suits, ranks);
+
+
+        cardValues.put(ACE, 11);   //TODO in order to have it be 1 or 11 we need to add 10 to the value if the total card value is <= 10
+        cardValues.put(TWO, 2);
+        cardValues.put(THREE, 3);
+        cardValues.put(FOUR, 4);
+        cardValues.put(FIVE, 5);
+        cardValues.put(SIX, 6);
+        cardValues.put(SEVEN, 7);
+        cardValues.put(EIGHT, 8);
+        cardValues.put(NINE, 9);
+        cardValues.put(TEN, 10);
+        cardValues.put(JACK, 10);
+        cardValues.put(QUEEN, 10);
+        cardValues.put(KING, 10);
+
     }
 
     public Deck getDeck() {
@@ -45,6 +65,13 @@ public class Game {
     public static String getRank(int rank) {
         return rankNames[rank];
     }
+    public Map<Integer, Integer> getCardValues(){
+        return cardValues;
+    }
+
+
+
+
 
 }
 
--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -20,6 +21,7 @@ public class Main {
             Deck deck = blackjack.getDeck();
             deck.shuffleDeck();
 
+
             List<Card> playerCards = deck.dealCards(2);
             List<Card> dealerCards = deck.dealCards(2);
 
@@ -35,7 +37,87 @@ public class Main {
             for (Card card : playerCards) {
                 System.out.println(Game.getRank(card.getRank()) + " " + Game.getSuit(card.getSuit()));
             }
-            //TODO: Implement entire game logic
+
+            Map<Integer, Integer> cardValues = blackjack.getCardValues();
+
+            // TODO figure out the stuff with ACES and the whole 1 and 11 thing in both dealerHit and whileHit loops
+
+            boolean willHit = true;
+            boolean dealerHit = true;
+            boolean bust = false;
+            int playerValue = 0;
+
+
+            while (willHit) {
+                playerValue = 0;
+                for (Card card : playerCards) {
+                    playerValue += cardValues.get(card.getRank());
+                    if (Game.getRank(card.getRank()).equals("Ace")) {
+                        // TODO we are here
+                    }
+                }
+                if (playerValue > 21) {
+                    System.out.println(playerValue + ": you bust!");
+                    dealerHit = false;
+                    bust = true;
+                    break;
+                }
+                if (playerValue == 21) {
+                    System.out.println("21, that's Blackjack!");
+                    break;
+                }
+
+                System.out.println("Player hand worth: " + playerValue);
+
+                System.out.println("Do you want to hit or stay?");
+                result = scan.next();
+                if (result.toLowerCase().substring(0, 1).equals("h")) {
+                    List<Card> newCard = deck.dealCards(1);
+                    Card hit = newCard.remove(0);
+                    System.out.println(Game.getRank(hit.getRank()) + " " + Game.getSuit(hit.getSuit()));
+                    playerCards.add(hit);
+                } else {
+                    willHit = false;
+                }
+            }
+
+            int dealerValue = 0;
+
+            while (dealerHit) {
+                System.out.println("Dealer:");
+
+                for (Card card : dealerCards) {
+                    System.out.println(Game.getRank(card.getRank()) + " " + Game.getSuit(card.getSuit()));
+                }
+                ;
+
+                dealerValue = 0;
+                for (Card card : dealerCards) {
+                    dealerValue += cardValues.get(card.getRank());
+                }
+                System.out.println("Dealer Value: " + dealerValue);
+
+                if (dealerValue < 17) {
+                    List<Card> newCard = deck.dealCards(1);
+                    Card hit = newCard.remove(0);
+                    System.out.println(Game.getRank(hit.getRank()) + " " + Game.getSuit(hit.getSuit()));
+                    dealerCards.add(hit);
+                } else {
+                    dealerHit = false;
+                }
+            }
+
+
+            if (!bust) {
+                if (dealerValue > playerValue) {
+                    System.out.println(dealerValue + " is higher than " + playerValue + ". You lose!");
+                } else if (playerValue > dealerValue) {
+                    System.out.println(playerValue + " is higher than " + dealerValue + ". You win!");
+                } else {
+                    System.out.println(playerValue + " is the same as " + dealerValue + ". You tie!");
+                }
+            }
+
 
             System.out.println("Do you want to play Blackjack again?");
             result = scan.next();
